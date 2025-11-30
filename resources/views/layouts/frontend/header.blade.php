dea8c75710b4861caef3cc612f875a35f3e3a160
shifat23 <shifat@revo-interactive.com>
2025-11-30 15:08:10 +0600

    the resource section is nor in hide state

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -97,31 +97,31 @@
                                 @endif
 
 
-                                @can('viewResourcesSection', Auth::user())
-                                    <li class="has-custom-submenu">
-                                        <a href="#">Resources
-                                            <i class="feather-chevron-down"></i>
-                                        </a>
-                                        @if(!empty($allResource))
-                                            <div class="custom-submenu">
-                                                @foreach($allResource as $subjectTitle => $resource)
-                                                    @if(!empty($resource))
-                                                        <div class="submenu-box">
-                                                            <div class="submenu-header">{{ $subjectTitle }}</div>
-                                                            <ul class="custom-submenu-list">
-                                                                @foreach($resource as $subject)
-                                                                    <li class="submenu-item">
-                                                                        <a href="{{ route('resources.topic', [$subject['education_level']['slug'], $subject['slug']]) }}">{{ $subject['education_level']['name'] }}</a>
-                                                                    </li>
-                                                                @endforeach
-                                                            </ul>
-                                                        </div>
-                                                    @endif
-                                                @endforeach
-                                            </div>
-                                        @endif
-                                    </li>
-                                @endcan
+{{--                                @can('viewResourcesSection', Auth::user())--}}
+{{--                                    <li class="has-custom-submenu">--}}
+{{--                                        <a href="#">Resources--}}
+{{--                                            <i class="feather-chevron-down"></i>--}}
+{{--                                        </a>--}}
+{{--                                        @if(!empty($allResource))--}}
+{{--                                            <div class="custom-submenu">--}}
+{{--                                                @foreach($allResource as $subjectTitle => $resource)--}}
+{{--                                                    @if(!empty($resource))--}}
+{{--                                                        <div class="submenu-box">--}}
+{{--                                                            <div class="submenu-header">{{ $subjectTitle }}</div>--}}
+{{--                                                            <ul class="custom-submenu-list">--}}
+{{--                                                                @foreach($resource as $subject)--}}
+{{--                                                                    <li class="submenu-item">--}}
+{{--                                                                        <a href="{{ route('resources.topic', [$subject['education_level']['slug'], $subject['slug']]) }}">{{ $subject['education_level']['name'] }}</a>--}}
+{{--                                                                    </li>--}}
+{{--                                                                @endforeach--}}
+{{--                                                            </ul>--}}
+{{--                                                        </div>--}}
+{{--                                                    @endif--}}
+{{--                                                @endforeach--}}
+{{--                                            </div>--}}
+{{--                                        @endif--}}
+{{--                                    </li>--}}
+{{--                                @endcan--}}
                                 @can('viewPastPaperSection', Auth::user())
                                     <li>
                                         <a href="{{ route('past.papers') }}">
